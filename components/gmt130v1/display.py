96a5413ee9b530512b152a02f8dff4bfb4d22312
Philippe RODIER <6016806+xfifi@users.noreply.github.com>
2023-10-22 15:07:47 +0200

    Update display.py

--- components/gmt130v1/display.py
+++ components/gmt130v1/display.py
@@ -8,13 +8,13 @@ from esphome.const import (
     CONF_ID,
     CONF_LAMBDA,
     CONF_WIDTH,
-    CONF_CS_PIN,
+    #CONF_CS_PIN,
     CONF_DC_PIN,
     CONF_RESET_PIN,
     CONF_NUMBER,
 )
 from esphome.const import __version__ as ESPHOME_VERSION
-from . import tdisplays3_ns
+from . import gmt130v1_ns
 
 AUTO_LOAD = ["psram"]
 DEPENDENCIES = ["esp32"]
@@ -24,30 +24,30 @@ CONF_LOAD_FONTS = "load_fonts"
 CONF_LOAD_SMOOTH_FONTS = "load_smooth_fonts"
 CONF_ENABLE_LIBRARY_WARNINGS = "enable_library_warnings"
 
-TDISPLAYS3 = tdisplays3_ns.class_(
-    "TDisplayS3", cg.PollingComponent, display.DisplayBuffer
+GMT130V1 = gmt130v1_ns.class_(
+    "gmt130v1", cg.PollingComponent, display.DisplayBuffer
 )
 
 CONFIG_SCHEMA = cv.All(
     display.FULL_DISPLAY_SCHEMA.extend(
         {
-            cv.GenerateID(): cv.declare_id(TDISPLAYS3),
-            cv.Optional(CONF_HEIGHT, default=320): cv.uint16_t,
-            cv.Optional(CONF_WIDTH, default=170): cv.uint16_t,
+            cv.GenerateID(): cv.declare_id(GMT130V1),
+            cv.Optional(CONF_HEIGHT, default=240): cv.uint16_t,
+            cv.Optional(CONF_WIDTH, default=240): cv.uint16_t,
             cv.Optional(CONF_BACKLIGHT, default=False): cv.boolean,
             cv.Optional(CONF_LOAD_FONTS, default=False): cv.boolean,
             cv.Optional(CONF_LOAD_SMOOTH_FONTS, default=False): cv.boolean,
             cv.Optional(CONF_ENABLE_LIBRARY_WARNINGS, default=False): cv.boolean,
-            cv.Optional(CONF_RESET_PIN, default=5): pins.gpio_output_pin_schema,
-            cv.Optional(CONF_CS_PIN, default=6): pins.gpio_output_pin_schema,
-            cv.Optional(CONF_DC_PIN, default=7): pins.gpio_output_pin_schema,
+            cv.Optional(CONF_RESET_PIN, default=4): pins.gpio_output_pin_schema,
+            #cv.Optional(CONF_CS_PIN, default=6): pins.gpio_output_pin_schema,
+            cv.Optional(CONF_DC_PIN, default=2): pins.gpio_output_pin_schema,
         }
     ).extend(cv.polling_component_schema("5s")),
 )
 
 
 async def to_code(config):
-    # Add platformio build_flags for the correct TFT_eSPI settings for the T-Display-S3
+    # Add platformio build_flags for the correct TFT_eSPI settings for the GMT130-V1.0 IPS 240x240 SPI display
     # This allows using current, unpatched versions of TFT_eSPI
     cg.add_build_flag("-DUSER_SETUP_LOADED")
     cg.add_build_flag("-DST7789_DRIVER")
@@ -55,22 +55,22 @@ async def to_code(config):
     cg.add_build_flag("-DCGRAM_OFFSET")
     cg.add_build_flag("-DTFT_RGB_ORDER=TFT_RGB")
     cg.add_build_flag("-DTFT_INVERSION_ON")
-    cg.add_build_flag("-DTFT_PARALLEL_8_BIT")
+    #cg.add_build_flag("-DTFT_PARALLEL_8_BIT")
     cg.add_build_flag(f"-DTFT_WIDTH={config[CONF_WIDTH]}")
     cg.add_build_flag(f"-DTFT_HEIGHT={config[CONF_HEIGHT]}")
     cg.add_build_flag(f"-DTFT_RST={config[CONF_RESET_PIN][CONF_NUMBER]}")
-    cg.add_build_flag(f"-DTFT_CS={config[CONF_CS_PIN][CONF_NUMBER]}")
+    #cg.add_build_flag(f"-DTFT_CS={config[CONF_CS_PIN][CONF_NUMBER]}")
     cg.add_build_flag(f"-DTFT_DC={config[CONF_DC_PIN][CONF_NUMBER]}")
-    cg.add_build_flag("-DTFT_WR=8")
-    cg.add_build_flag("-DTFT_RD=9")
-    cg.add_build_flag("-DTFT_D0=39")
-    cg.add_build_flag("-DTFT_D1=40")
-    cg.add_build_flag("-DTFT_D2=41")
-    cg.add_build_flag("-DTFT_D3=42")
-    cg.add_build_flag("-DTFT_D4=45")
-    cg.add_build_flag("-DTFT_D5=46")
-    cg.add_build_flag("-DTFT_D6=47")
-    cg.add_build_flag("-DTFT_D7=48")
+    #cg.add_build_flag("-DTFT_WR=8")
+    #cg.add_build_flag("-DTFT_RD=9")
+    #cg.add_build_flag("-DTFT_D0=39")
+    #cg.add_build_flag("-DTFT_D1=40")
+    #cg.add_build_flag("-DTFT_D2=41")
+    #cg.add_build_flag("-DTFT_D3=42")
+    #cg.add_build_flag("-DTFT_D4=45")
+    #cg.add_build_flag("-DTFT_D5=46")
+    #cg.add_build_flag("-DTFT_D6=47")
+    #cg.add_build_flag("-DTFT_D7=48")
 
     if config[CONF_LOAD_FONTS]:
         cg.add_build_flag("-DLOAD_GLCD")
